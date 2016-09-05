b95636e2a6567011cacd65ab1622b847d912b772
Bartłomiej Rapacz <tsiruu@gmail.com>
2016-09-05 16:47:44 +0200

    Fixed visitors page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :visitors
   resources :students do
     get :subjects
   end
   resources :teachers do
     get :subjects
   end
+
   resources :report_subjects
 
-  root to: 'students#index'
+  root to: 'visitors#index'
 end
