3f9c42218fe8cfde866e908060d4742fcd1252a8
Bartłomiej Rapacz <tsiruu@gmail.com>
2016-09-05 15:26:34 +0200

    Fixed decorators for student and teacher model

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,13 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    '%0.2f' % mean(subject_item.subject_item_notes.pluck(:value))
+  end
+
+  private
+
+  def mean(list)
+    return 0 if list.empty?
+    list.reduce(0.00, :+) / list.count
   end
 end
